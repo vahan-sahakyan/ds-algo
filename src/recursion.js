5088e4f157bc6d1e76fce97f82ee0b876a7edf1f
Vahan Sahakyan <vahan.sahakyan1996@gmail.com>
2023-02-05 23:12:08 +0400

    figured out insertion sort, started merge sort

--- src/recursion.js
+++ src/recursion.js
@@ -1,14 +1,5 @@
-// function findFactorialRecursive(n) {
-//   if (n === 2) return 2;
-//   return n * findFactorialRecursive(n - 1);
-// }
-// console.log(findFactorialRecursive(5));
-
-// function fib(n) {
-//   if (n < 2) return n;
-//   return fib(n - 2) + fib(n - 1);
-// }
-const fib = n => (n < 2 ? n : fib(n - 2) + fib(n - 1));
-
-for (let i = 0; i < 1000; i++) console.log(fib(i));
-// console.log(fib(40));
+function fib(n) {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+}
+console.log(fib(8));
--- src/sort.js
+++ src/sort.js
@@ -1,4 +1,85 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+//
+//
+//
+//
+//
+//
+//
+function mergeSort(array) {
+  if (array.length === 1) return array;
+
+  // split array in into right and left
+  const middle = Math.floor(array.length / 2);
+  const left = array.slice(0, middle),
+    right = array.slice(middle);
+
+  console.log({ left, right });
+  return merge(mergeSort(left), mergeSort(right));
+}
+function merge(L, R) {
+  // console.log({ L, R });
+}
+const result = mergeSort(numbers);
+
+// for (n of result) console.log(n);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    // put smallest first
+    if (array[i] < array[0]) {
+      array.unshift(array.splice(i, 1)[0]);
+    } else {
+      // loop in left side and find the spot that fits, if bigger - nothing fits, keep the same place
+      for (let j = 1; j < i; j++) {
+        if (array[i] > array[j - 1] && array[i] < array[j]) {
+          array.splice(j, 0, array.splice(i, 1)[0]);
+        }
+      }
+    }
+  }
+}
+// insertionSort(numbers);
+// for (n of numbers) console.log(n);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 function bubbleSort(arr) {
   const length = arr.length;
   for (let i = 0; i < length; i++) {
@@ -9,5 +90,5 @@ function bubbleSort(arr) {
     }
   }
 }
-bubbleSort(numbers);
-console.log(...numbers);
+// bubbleSort(numbers);
+// for (n of numbers) console.log(n);
